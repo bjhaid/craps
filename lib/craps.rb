b7045a15c6ba7fc950acf96146800d8d5b9c2397
Abejide Ayodele <abejideayodele@gmail.com>
2013-08-28 18:14:32 -0500

    rewritten craps

--- lib/craps.rb
+++ lib/craps.rb
@@ -3,60 +3,62 @@ class Die
     rand(1..6)
   end
 end
- 
-die1 = Die.new
-die2 = Die.new
-# puts dice_roll = die1.roll + die2.roll
-first_roll = true
- 
-class Player
- 
-  def initialize(output)
-    @point = nil
-    @output = output
+
+
+class Craps
+  attr_accessor :first_die, :second_die
+
+  def initialize
+    @turn_count = 1
+    @point_from_first_turn
+    @playing = true
   end
- 
-  def pass dice_roll
-    (dice_roll == 7 || dice_roll == 11)
+
+  def first_die_roll
+    first_die.roll
   end
- 
-  def no_pass dice_roll
-    dice_roll == 2 || dice_roll == 3 || dice_roll == 12
+
+  def second_die_roll
+    second_die.roll
   end
- 
-  def roll (die1, die2, first_roll)
-    dice_roll = die1.roll + die2.roll
-    if first_roll
-      if pass (dice_roll)
-        output.puts dice_roll
-        output.puts "'natural' roll, you WIN!"
-      elsif no_pass(dice_roll)
-        # output.puts dice_roll
-        output.puts "'craps!' you LOSE!"
-      else
-        @point = dice_roll
-        puts "set point is #{@point}"
-        die1 = Die.new
-        die2 = Die.new
-        roll(die1, die2, false)
-      end
-    else
-      puts "current roll is #{dice_roll}"
-      if dice_roll == 7
-        puts "you crapped out with a #{dice_roll}"
-      elsif dice_roll == @point
-        puts "you win with #{@point}"
-      else
-        die1 = Die.new
-        die2 = Die.new
-        roll(die1, die2, false)
-      end
+
+  def first_roll?
+    @turn_count == 1
+  end
+
+  def roll
+    first_die_roll + second_die_roll
+  end
+
+  def turn
+    while @playing
+      response = status
+      puts "You #{response} with points #{@point} at turn no #{@turn_count}"
+      @turn_count += 1
+    end
+    response
+  end
+
+  def status
+    @point = roll
+    if first_roll? && [7,11].include?(@point)
+      @playing = false
+      return 'Win!!!'
+    elsif first_roll? && [2,3,12].include?(@point)
+      @playing = false
+      return "Craps you loose"
+    elsif first_roll?
+      @point_from_first_turn ||= @point
+      "Retry"
+    elsif !first_roll? && [7,11].include?(@point)
+      @playing = false
+      'craps out!?'
+    elsif !first_roll? && @point_from_first_turn == @point
+      @playing = false
+      'Win!!!'
+    elsif !first_roll? && @point_from_first_turn != @point
+      "Retry"
     end
   end
 
-  private
-
-  attr_accessor :output
 end
- 
-# Player.new.roll(die1, die2, true)
--- spec/craps_spec.rb
+++ spec/craps_spec.rb
@@ -1,29 +1,116 @@
 require 'craps'
 
-describe Player do
+describe Craps do
+  let(:craps) { Craps.new }
   it "wins if rolls a 7 on the first roll" do
-    output = double('output').as_null_object
-    die1 = Die.new
-    die1.stub(:roll).and_return(3)
-    die2 = Die.new
-    die2.stub(:roll).and_return(4)
-    player = Player.new(output)
-
-    player.roll(die1, die2, true)
-
-    expect(output).to have_received(:puts).with("'natural' roll, you WIN!")
+    craps.stub(:first_die_roll).and_return(3)
+    craps.stub(:second_die_roll).and_return(4)
+    expect(craps.roll).to eq 7
   end
 
-  it "wins if rolls a 3 on the first roll" do
-    output = double('output').as_null_object
-    die1 = Die.new
-    die1.stub(:roll).and_return(1)
-    die2 = Die.new
-    die2.stub(:roll).and_return(2)
-    player = Player.new(output)
+  it "expects first_roll should be true" do
+    expect(craps.first_roll?).to be_true
+  end
 
-    player.roll(die1, die2, true)
+  it "expects first_roll should be false if turn_count is not 1" do
+    craps.instance_variable_set(:@turn_count, 2)
+    expect(craps.first_roll?).to be_false
+  end
 
-    expect(output).to have_received(:puts).with("'craps!' you LOSE!")
+  it "expect a 11 on the first roll" do
+    craps.stub(:first_die_roll).and_return(5)
+    craps.stub(:second_die_roll).and_return(6)
+    expect(craps.roll).to eq 11
+  end
+
+  it "status should be 'Win!!!'' if it is 11 on first_roll" do
+    craps.stub(:first_die_roll).and_return(5)
+    craps.stub(:second_die_roll).and_return(6)
+    expect(craps.status).to eq 'Win!!!'
+  end
+
+  it "status should be 'Win!!!'' if it is 7 on first_roll" do
+    craps.stub(:first_die_roll).and_return(1)
+    craps.stub(:second_die_roll).and_return(6)
+    expect(craps.status).to eq 'Win!!!'
+  end
+
+  it "status should be 'Craps you loose' if it is 2" do
+    craps.stub(:first_die_roll).and_return(1)
+    craps.stub(:second_die_roll).and_return(1)
+    expect(craps.status).to eq 'Craps you loose'
+  end
+
+  it "status should be 'Craps you loose' if it is 3" do
+    craps.stub(:first_die_roll).and_return(1)
+    craps.stub(:second_die_roll).and_return(2)
+    expect(craps.status).to eq 'Craps you loose'
+  end
+
+  it "status should be 'Craps you loose' if it is 12" do
+    craps.stub(:first_die_roll).and_return(6)
+    craps.stub(:second_die_roll).and_return(6)
+    expect(craps.status).to eq 'Craps you loose'
+  end
+
+  it "status should be 'craps out!?', if  7 on a turn other than the first turn" do
+    craps.instance_variable_set(:@turn_count, 2)
+    craps.stub(:first_die_roll).and_return(1)
+    craps.stub(:second_die_roll).and_return(6)
+    expect(craps.status).to eq 'craps out!?'
+  end
+
+  it "status should be 'craps out!?', if  11 on a turn other than the first turn" do
+    craps.instance_variable_set(:@turn_count, 2)
+    craps.stub(:first_die_roll).and_return(5)
+    craps.stub(:second_die_roll).and_return(6)
+    expect(craps.status).to eq 'craps out!?'
+  end
+
+  it "status should be 'Win!!!'' if  point_from_first_turn equals current roll" do
+    craps.instance_variable_set(:@turn_count, 7)
+    craps.instance_variable_set(:@point_from_first_turn, 6)
+    craps.stub(:first_die_roll).and_return(5)
+    craps.stub(:second_die_roll).and_return(1)
+    expect(craps.status).to eq 'Win!!!'
+  end
+
+  it "status should be 'Retry' if roll is not 7,11 and roll is not equal to point_from_first_turn" do
+    craps.instance_variable_set(:@turn_count, 5)
+    craps.instance_variable_set(:@point_from_first_turn, 8)
+    craps.stub(:first_die_roll).and_return(2)
+    craps.stub(:second_die_roll).and_return(4)
+    expect(craps.status).to eq 'Retry'
+  end
+
+  it "status should return 'craps out!?' if point_from_first_turn = 6 and roll = 7" do
+    craps.instance_variable_set(:@turn_count, 5)
+    craps.instance_variable_set(:@point_from_first_turn, 6)
+    craps.stub(:first_die_roll).and_return(3)
+    craps.stub(:second_die_roll).and_return(4)
+    expect(craps.status).to eq 'craps out!?'
+  end
+
+  it "status should return 'craps out!?' if point_from_first_turn = 5 and roll = 11" do
+    craps.instance_variable_set(:@turn_count, 19)
+    craps.instance_variable_set(:@point_from_first_turn, 5)
+    craps.stub(:first_die_roll).and_return(5)
+    craps.stub(:second_die_roll).and_return(6)
+    expect(craps.status).to eq 'craps out!?'
+  end
+
+  describe "Testing playing" do
+    it "should return a string" do
+      craps.first_die = Die.new
+      craps.second_die = Die.new
+      turn = craps.turn
+      array_of_expected_responses = ['Win!!!', 'Craps you loose', 'craps out!?']
+      array_of_expected_responses.should include turn
+    end
+
+    it "expect @playing to be default to false" do
+      expect(craps.instance_variable_get(:@playing)).to be_true
+    end
+
   end
 end
